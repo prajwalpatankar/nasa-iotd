6519743da6373b4fe8c19b7a7b19262baa8f8d54
prajwalpatankar <patankarprajwal@gmail.com>
2023-04-06 15:52:52 -0700

    db change

--- backend/database.js
+++ backend/database.js
@@ -1,49 +1,3 @@
-// const { MongoClient } = require('mongodb');
-
-// const url = process.env.MONGODB_URI;
-// const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// async function connectToDatabase() {
-//     try {
-//         await client.connect();
-//         console.log('Connected to MongoDB');
-
-//         const db = client.db(process.env.DB_NAME);
-//         const users = db.collection('users');
-
-//         // Define User model
-//         class User {
-//             constructor(username, password) {
-//                 this.username = username;
-//                 this.password = password;
-//             }
-//         }
-
-//         // Define methods for User model
-//         User.findByUsername = async function (username) {
-//             const user = await users.findOne({ username: username });
-//             return user ? new User(user.username, user.password) : null;
-//         };
-
-//         User.create = async function (username, password) {
-//             const result = await users.insertOne({ username: username, password: password });
-//             return result.insertedId;
-//         };
-
-//         return {
-//             User: User,
-//             closeConnection: async function () {
-//                 await client.close();
-//                 console.log('Disconnected from MongoDB');
-//             },
-//         };
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB', error);
-//     }
-// }
-
-// module.exports = { connectToDatabase };
-
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
@@ -52,7 +6,7 @@ require('dotenv').config();
 const url = process.env.MONGODB_URI;
 const client = new MongoClient(url, { useUnifiedTopology: true });
 
-async function connectToDatabase() {
+module.exports = async function connectToDatabase() {
     try {
         await client.connect();
         console.log('Connected to MongoDB');
@@ -91,5 +45,5 @@ async function connectToDatabase() {
     }
 }
 
-module.exports = { connectToDatabase };
+// module.exports = { connectToDatabase };
 
