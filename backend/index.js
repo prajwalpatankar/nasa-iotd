e74a8a8ef844e24b3fe682740c896aaeae77a828
prajwalpatankar <patankarprajwal@gmail.com>
2023-04-06 16:03:29 -0700

    index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const app = express()
-const { connectToDatabase } = require('./database');
 
 const cors = require('cors')
 app.use(cors())
@@ -11,7 +10,54 @@ app.use(bodyParser.json());
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
+
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
 const secret = process.env.TOKEN_SECRET;
+const url = process.env.MONGODB_URI;
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        const db = client.db("myminternship");
+        const users = db.collection('user_details');
+
+        // Define User model
+        class UserDetails {
+            constructor(username, password) {
+                this.username = username;
+                this.password = password;
+            }
+        }
+
+        // Define methods for User model
+        UserDetails.findByUsername = async function (username) {
+            const user = await users.findOne({ username: username });
+            return user ? new UserDetails(user.username, user.password) : null;
+        };
+
+        UserDetails.create = async function (username, password) {
+            const result = await users.insertOne({ username: username, password: password });
+            return result.insertedId;
+        };
+
+        return {
+            UserDetails: UserDetails,
+            closeConnection: async function () {
+                await client.close();
+                console.log('Disconnected from MongoDB');
+            },
+        };
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+    }
+}
+
 
 app.post("/signup", async (req, res) => {
     const { username, password } = req.body;
